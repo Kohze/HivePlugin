0aa453f0a7a277e52b766baab5c04f950aba52d8
Daniel Zigerlig <daniel.zigerlig@swisscom.com>
2019-09-17 18:33:18 +0200

    removed FocusGraph component, now implementing all in App.js for now

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import {makeStyles} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import FocusGraph from './components/FocusGraph';
+import {ForceGraph3D} from "react-force-graph";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -24,6 +24,33 @@ const useStyles = makeStyles(theme => ({
 
 export default function App() {
     const classes = useStyles();
+
+    const [fg, setFg] = useState(0);
+
+    const hiveData = {
+        nodes: [
+            { group: 1, id: 1, title: "Ideas Born in the BSV Hackathon with Carl Jackett" },
+            { group: 2, id: 2, title: "London Meetup" },
+            { group: 3, id: 3, title: "BSV Alias" },
+            { group: 4, id: 4, title: "The Power of Simple with Wei Zhang" }
+        ],
+        links: [
+            {source: 1, target: 2, curvature: 0.8, rotation: Math.PI * 1 / 6 },
+            {source: 3, target: 2, curvature: 0.8, rotation: Math.PI * 1 / 6 },
+            {source: 4, target: 2, curvature: 0.8, rotation: Math.PI * 1 / 6 }
+        ]
+    };
+
+    function _handleClick(node) {
+        // Aim at node from outside it
+        const distance = 40;
+        const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+        fg.cameraPosition(
+            { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
+            node, // lookAt ({ x, y, z })
+            3000  // ms transition duration
+        );
+    }
 
     return (
         <div
@@ -46,7 +73,21 @@ export default function App() {
                 {/*  {'The footer will move as the main element of the page grows.'}*/}
                 {/*</Typography>*/}
                 {/*<Typography variant="body1">Sticky footer placeholder.</Typography>*/}
-                <FocusGraph />
+                <ForceGraph3D
+                    height={700}
+                    width={1000}
+                    graphData={hiveData}
+                    nodeAutoColorBy="group"
+                    nodeLabel="title"
+                    linkDirectionalArrowLength={3.5}
+                    linkDirectionalArrowRelPos={1}
+                    linkWidth={2}
+                    linkCurvature="curvature"
+                    linkCurveRotation="rotation"
+                    linkDirectionalParticles={2}
+                    ref={el => { setFg(el) }}
+                    onNodeClick={_handleClick}
+                />
 
             </Container>
             <footer className={classes.footer}>
